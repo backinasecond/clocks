8c1e03ebd6a8e49f5c045dd0c33c922cd75d6ec5
doodspav <doods9doods@gmail.com>
2020-05-10 00:51:14 +0100

    Create clock_24.hpp

---
+++ include/clock_24.hpp
@@ -0,0 +1,13 @@
+#ifndef CLOCKS_CLOCK_12_HPP
+#define CLOCKS_CLOCK_12_HPP
+
+namespace clocks {
+
+    struct clock_24
+    {
+        static constexpr int N = 24;
+    };
+
+}
+
+#endif //!CLOCKS_CLOCK_12_HPP
